e92f785eb3a4ab65db0fdfbf75ce007cd3504b4a
EngRazanBalatiah <eng.razanBalatiah96@gmail.com>
2026-04-06 19:32:30 +0300

    remove the line

--- cypress/e2e/smokeTests/telerik.cy.js
+++ cypress/e2e/smokeTests/telerik.cy.js
@@ -9,6 +9,5 @@ describe('Selectors in Telerik', () => {
         cy.get("[alt=Google-play-icon]")
         cy.get(".TK-Footer-List-Social").first().children()
         cy.get(".TK-Footer-List-Social").first().find("*").filter("li") 
-        // test
     });
 });
